b66e3950283b96b113ffe60e1dbec6a51f347fa0
moonix0 <moongraph21@gmail.com>
2023-05-26 00:30:32 +0700

    Fixing button filter

--- src/components/ButtonFilter.jsx
+++ src/components/ButtonFilter.jsx
@@ -2,7 +2,7 @@ const ButtonFilter = ({ name, action }) => {
   return (
     <button
       onClick={action}
-      className='py-2 px-3.5 bg-slate-400 rounded-md text-white hover:bg-blue-300 focus:bg-blue-500'
+      className='py-2 px-3.5 bg-slate-400 rounded-sm text-white hover:bg-blue-300 focus:bg-blue-500'
     >
       {name}
     </button>
